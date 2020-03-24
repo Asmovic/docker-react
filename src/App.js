5503f86304bcf72901d0e08e8066575bb42988a0
asmovic <owoyemifood@gmail.com>
2020-03-24 19:07:11 +0100

    alhamdulilahi robil aleymin

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <i>Hello Movic.</i> <b>Alhamdulilahi robil aleymin.</b><b> Allahu Akbar.</b>
+          <i>Hello Movic.</i> <strong>Subhanalahi </strong><b>Alhamdulilahi robil aleymin.</b><b> Allahu Akbar.</b>
         </p>
         <a
           className="App-link"
