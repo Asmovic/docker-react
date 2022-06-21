0cb826bebe6ecedb0f5f74d1a069dc042665c7e1
Asmovic <asogba007@gmail.com>
2022-06-21 13:03:38 +0100

    Clean App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,28 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-/* 
-const App = () => {
-  return <div>Hi</div>
-} */
 
-it('renders without crashing', () => {
-  expect(true).toEqual(true);
-  /* const div = document.createElement('div');
-
-  ReactDOM.render(<App />, div);
-  expect(div.innerHTML).toContain('Hi');
-  ReactDOM.unmountComponentAtNode(div); */
-})
-
-/* import { render } from '@testing-library/react';
-import App from './App'; */
-
-/* test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-}); */
+it('renders without crashing', () => {});
 
 
 
